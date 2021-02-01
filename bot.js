b2ddc40e87406a0946ee3df8c82c1bec791add03
kirtirajsinh <kirtiraj3738@gmail.com>
2021-02-01 18:24:31 +0000

    added an embed message

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const api_key = process.env.API_KEY;
+const Discord = require('discord.js');
 // URL for Bitcoin API
 const btc_url = `https://api.nomics.com/v1/currencies/ticker?key=${api_key}&ids=BTC&interval=1h,1d&convert=USD&per-page=100&page=1`;
 // URL for etherium URL
@@ -27,8 +28,29 @@ const btc = module.exports = async (msg) => {
         const btcresponse = await fetch(btc_url);
         let btcjson = await btcresponse.json();
         console.log(btcjson)
-        //sending response to the user
-        msg.channel.send(response);
+        const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#f2a900')
+	.setTitle('Crypto BOT')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Crypto BOT', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail(btcjson[0].logo_url)
+	.addFields(
+		{ name: 'Currency', value: btcjson[0].currency },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Price', value: btcjson[0].price , inline: true },
+		{ name: 'Market cap', value: btcjson[0].market_cap , inline: true },
+    { name: 'Price change in 1hour', value: btcjson[0].price_change },
+    { name: 'first Trade', value: btcjson[0].first_trade }
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        
+        msg.channel.send(exampleEmbed);
+          
+        
         
       }
       //if ther's some error 
@@ -44,7 +66,7 @@ const btc = module.exports = async (msg) => {
         const ethjson = await ethresponse.json();
         console.log(ethjson);
         //sending eth response
-        msg.channel.send(response);
+        msg.channel.send(ethjson);
       }
       catch (err){
         console.error(err);
@@ -58,10 +80,7 @@ const btc = module.exports = async (msg) => {
     
   }
 }
-// const response = {
-//           id:this.id,
-//           currency:this.currency
-//         }
+
 
 
 
